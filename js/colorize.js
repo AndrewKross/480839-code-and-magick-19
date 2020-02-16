ff578854b29fa2557ca2afa96aab6c877dad8157
Andrew <andkross@yandex.ru>
2020-02-16 03:29:07 +0300

    добавлена сортировка похожих волшебников

--- js/colorize.js
+++ js/colorize.js
@@ -10,20 +10,45 @@
     '#e6e848'
   ];
 
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'];
+
+  var EYES_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
+
   var setup = document.querySelector('.setup');
   var setupPlayer = document.querySelector('.setup-player');
-  var wizardCoat = setupPlayer.querySelector('.wizard-coat');
-  var wizardEyes = setupPlayer.querySelector('.wizard-eyes');
+  var wizardCoatElement = setupPlayer.querySelector('.wizard-coat');
+  var wizardEyesElement = setupPlayer.querySelector('.wizard-eyes');
   var setupFireball = setup.querySelector('.setup-fireball-wrap');
 
-  wizardCoat.addEventListener('click', function () {
-    wizardCoat.style.fill = window.wizard.COAT_COLORS[window.util.getRandom(window.wizard.COAT_COLORS)];
-    setupPlayer.querySelector('input[name="coat-color"]').value = wizardCoat.style.fill;
+  var colorize = {
+    onEyesChange: function () {},
+    onCoatChange: function () {}
+  };
+
+  wizardCoatElement.addEventListener('click', function () {
+    var newColor = COAT_COLORS[window.util.getRandom(COAT_COLORS)];
+    wizardCoatElement.style.fill = newColor;
+    setupPlayer.querySelector('input[name="coat-color"]').value = wizardCoatElement.style.fill;
+    colorize.onCoatChange(newColor);
   });
 
-  wizardEyes.addEventListener('click', function () {
-    wizardEyes.style.fill = window.wizard.EYES_COLORS[window.util.getRandom(window.wizard.EYES_COLORS)];
-    setupPlayer.querySelector('input[name="eyes-color"]').value = wizardEyes.style.fill;
+  wizardEyesElement.addEventListener('click', function () {
+    var newColor = EYES_COLORS[window.util.getRandom(EYES_COLORS)];
+    wizardEyesElement.style.fill = newColor;
+    setupPlayer.querySelector('input[name="eyes-color"]').value = wizardEyesElement.style.fill;
+    colorize.onEyesChange(newColor);
   });
 
   setupFireball.addEventListener('click', function () {
@@ -31,5 +56,7 @@
     setupFireball.style.background = randomRgbFireballColor;
     setupFireball.querySelector('input').value = randomRgbFireballColor;
   });
+
+  window.colorize = colorize;
 
 })();
